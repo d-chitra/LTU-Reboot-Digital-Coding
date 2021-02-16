2165fa03bbb170d0fbed16edabfcf27ec38e9e3f
d-chitra <71097403+d-chitra@users.noreply.github.com>
2021-02-16 19:43:01 +0000

    adding argument to function

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,13 @@
+function getfullName(firstName,lastName){
+    let fullName = firstName + ' ' + lastName;
+    console.log(fullName);
+}
+
+var fName = 'Chitra';
+var lName ='Deokar';
+
+var fName2 = 'Shrija';
+var lName2 = 'Pawar';
+
+getfullName(fName,lName);
+getfullName(fName2,lName2);
