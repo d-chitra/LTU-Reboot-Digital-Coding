6c917543a2ae8a3e2a4b98dd9ea34278f834d423
d-chitra <71097403+d-chitra@users.noreply.github.com>
2021-02-18 20:36:05 +0000

    function calculator code

---
+++ module-3/lesson-3/scripts/task-3.3.3.js
@@ -0,0 +1,37 @@
+function isValidNumber(number){
+    return !isNaN(number);
+}
+
+function calculator(number1, number2, operator){
+    strMessage = '';
+    if (!isValidNumber(number1)){
+        return 'Argument 1 must be number';
+    }
+    if (!isValidNumber(number2)){
+        return 'Argument 2 must be number';
+    }
+    if (operator != '+' && operator != '-' && operator != '*' && operator != '/' && operator != '%'){
+        return 'Argument 3 must be arithmatic operator';
+    }
+    var sum;
+    switch (operator){
+    case '+':
+        sum = number1 + number2;
+    break;
+    case '-':
+        sum = number1 - number2;
+    break;
+    case '*':
+        sum = number1 * number2;
+    break;
+    case '/':
+        sum = number1 / number2;
+    break;
+    case '%':
+        sum = number1 % number2;
+    break;
+}
+    return sum;
+}
+sum = calculator(10, 10, '+');
+console.log(sum);
