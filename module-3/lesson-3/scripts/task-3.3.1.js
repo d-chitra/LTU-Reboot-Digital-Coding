c6359ddbe7d34fc776d1d86987cfb05a57e07e65
d-chitra <71097403+d-chitra@users.noreply.github.com>
2021-02-18 19:23:50 +0000

    percentage calculator code

---
+++ module-3/lesson-3/scripts/task-3.3.1.js
@@ -0,0 +1,27 @@
+function percentageCalculator(number,percentage){
+    var blnValidated = true;
+    if(isNaN(number)){
+        blnValidated = false;
+    }
+    if(isNaN(number)){
+        blnValidated = true;
+          }
+    if(blnValidated) {
+        onePercent = number / 100;
+        percentValue = onePercent * percentage;
+        return percentValue;
+
+    }
+    else {
+        console.log('both arguments needs to be a number')
+    }
+}
+
+var sum = percentageCalculator(100,90);
+
+
+console.log(sum)
+
+
+
+
