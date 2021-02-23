577abb5b3e6637dd513968c734b26db2dd425349
d-chitra <71097403+d-chitra@users.noreply.github.com>
2021-02-23 19:39:56 +0000

    7 multiplier table till 12 code

---
+++ module-3/lesson-4/scripts/task-3.4.1.js
@@ -0,0 +1,11 @@
+multiplyer = 7;
+
+for(
+    counter=1;
+    counter<=12;
+    counter++
+){
+    let sum = multiplyer * counter;
+    let message = multiplyer + '*' + counter + ' = ' + sum;
+    console.log(message);
+}
