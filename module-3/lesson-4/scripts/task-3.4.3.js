941baf3fb5d488bf570141e9b1053cac37d27d62
d-chitra <71097403+d-chitra@users.noreply.github.com>
2021-02-23 20:33:20 +0000

    food list code using loop, array

---
+++ module-3/lesson-4/scripts/task-3.4.3.js
@@ -0,0 +1,6 @@
+var arrFavouritefoods =['Rice', 'Pasta', 'Fish', 'Sandwich', 'Panipuri', 'Samosa'];
+
+for(var counter = 0;counter<arrFavouritefoods.length; counter++){
+    let food = arrFavouritefoods[counter];
+    console.log(counter + ':' + food);
+}
