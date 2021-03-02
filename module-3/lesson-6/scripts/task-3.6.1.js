1514f51260f3641d6b4d30bbb0925b77148b5c6a
d-chitra <71097403+d-chitra@users.noreply.github.com>
2021-03-02 19:50:54 +0000

    fix

--- module-3/lesson-6/scripts/task-3.6.1.js
+++ module-3/lesson-6/scripts/task-3.6.1.js
@@ -2,4 +2,4 @@ var element = document.querySelector('.selectMe');
 console.log(element);
 element.setAttribute('alex','example');
 var h1 = document.querySelector("h1");
-h1.style.fontSize = '100px';
+h1.style.fontSize = "100px"
---
+++ module-3/lesson-6/scripts/task-3.6.2.js
@@ -0,0 +1,5 @@
+var newElement = document.createElement('p');
+console.log(newElement);
+var divElement = document.querySelector('div');
+//console.log(divElement);
+newElement.innerHTML = 'Chitra is cool';
